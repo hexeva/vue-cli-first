1eb38f6683b3f5b0cfc97217bf84e776c6231b3d
hexeva <patrick79.g@gmail.com>
2021-12-17 15:59:36 +0100

    push

---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+             <h1>La mia prima app con Vue Cli</h1>
+      
+    </section>
+</template>
+
+<script>
+export default {
+  name: "Title"
+  
+};
+</script>
+
+<style scoped>
+h1{
+    text-transform: uppercase;
+    color:red;
+    text-align: center;
+    margin: 30px 0;
+    font-size: 40px;
+}
+
+</style>
